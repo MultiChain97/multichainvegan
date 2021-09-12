681a298f6b52cd1047d3ce267cb009486cdaab57
Multichain97 <joshimaahi51@gmail.com>
2021-09-12 04:48:05 -0700

    je

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-unresolved
 import { PancakeTheme } from '@pancakeswap-libs/uikit/dist/theme'
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@veganswap-libs/uikit'
+
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
